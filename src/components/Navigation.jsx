53d66bf2ae483068e6b10023d7b43be6a64d46c3
Vito <vitohartanto@mail.ugm.ac.id>
2024-04-01 18:25:41 +0700

    adding login, logout, and register via Contact API

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
-import { FiHome, FiPlusCircle } from "react-icons/fi";
+import { FiHome, FiLogOut, FiPlusCircle } from "react-icons/fi";
+import PropTypes from "prop-types";
 
-const Navigation = () => {
+const Navigation = ({ logout, name }) => {
   return (
     <nav className="navigation">
       <ul>
@@ -15,9 +16,20 @@ const Navigation = () => {
             <FiPlusCircle />
           </Link>
         </li>
+        <li>
+          <button onClick={logout}>
+            {name}
+            <FiLogOut />
+          </button>
+        </li>
       </ul>
     </nav>
   );
+};
+
+Navigation.propTypes = {
+  logout: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default Navigation;
